e57fbbe0feacd1b87d873aa327c3fa820f75c5cc
Ann <miannmhartman@gmail.com>
2022-06-09 18:41:22 -0700

    Creating GET and POST

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require ('express')
 const app = express()
 
+
+app.use(express.json())  // this will allow application to accept JSON 
+
 const users = []
 
+// GET
 app.get('/users', (req, res) => {
-    res.json()
+    res.json(users)
 })
+
+// POST
+app.post('/users', (req, res) => {
+    const user = { name: req.body.name, password: req.body.password }
+    users.push(user)
+    res.status(201).send()
+})
+
 
 app.listen(3000)
