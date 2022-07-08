d270a9c1a6dc1812b5da196946715228307f8614
Ann <miannmhartman@gmail.com>
2022-07-08 14:33:59 -0700

    salt and hash password with bcrypt

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require ('express')
 const app = express()
+const bcrypt = require('bcrypt') 
 
 
 app.use(express.json())  // this will allow application to accept JSON 
 
-const users = []
+const users = [] // You can add a name [{name: 'Name'}]
 
 // GET
 app.get('/users', (req, res) => {
@@ -12,11 +13,22 @@ app.get('/users', (req, res) => {
 })
 
 // POST
-app.post('/users', (req, res) => {
-    const user = { name: req.body.name, password: req.body.password }
-    users.push(user)
-    res.status(201).send()
+app.post('/users', async (req, res) => {
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10) // append with salt
+    // put directly into the users 
+        const user = { name: req.body.name, password: hashedPassword} // salt is already saved inside hashedPassword 
+    
+    users.push(user) 
+    res.status(201).send() // sent blank response to the user
+    }
+
+    catch { //  in case something goes wrong we can just set a status
+        res.status(500).send()
+    }
+
 })
 
-
+//  LISTEN
 app.listen(3000)
