36cc0012cda3f93e5db6d72785de9001f26b98ae
Ann <miannmhartman@gmail.com>
2022-07-08 16:56:16 -0700

    Add POST users/login

--- server.js
+++ server.js
@@ -27,8 +27,29 @@ app.post('/users', async (req, res) => {
     catch { //  in case something goes wrong we can just set a status
         res.status(500).send()
     }
-
 })
+
+//LOGIN
+app.post('/users/login', async (req, res) => {
+        const user = users.find(user => user.name === req.body.name)
+    if (user == null) {
+        return res.status(400).send('Cannot find user')
+    }
+
+    try {
+        if(await bcrypt.compare(req.body.password, user.password)) {
+          res.send('Success')
+        }
+    else {
+        res.send('Not Allowed')
+    }
+    }
+
+    catch { //  in case something goes wrong we can just set a status
+        res.status(500).send()
+    }
+})
+
 
 //  LISTEN
 app.listen(3000)
